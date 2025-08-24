62fc26b0a517570da5b54347b66048227968de9b
first try <tamarhain3@gmail.com>
2025-08-24 15:45:22 +0300

    mines flag

--- screen.py
+++ screen.py
@@ -1,18 +1,24 @@
 import pygame
+import random
+
 
 def screen():
-    background_colour = (51, 102, 0)
-    screen = pygame.display.set_mode((800, 500))
-    pygame.display.set_caption('The Flag')
-    screen.fill(background_colour)
-    pygame.display.flip()
-    pygame.init()
-    img = pygame.image.load('grass.png')
-    IMAGE_SMALL = pygame.transform.scale(img, (50, 30))
-    running = True
-    while running:
-        screen.blit(IMAGE_SMALL, (0, 0))
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+   background_colour = (51, 102, 0)
+   screen = pygame.display.set_mode((800, 500))
+   pygame.display.set_caption('The Flag')
+   screen.fill(background_colour)
+   pygame.display.flip()
+   pygame.init()
+   img = pygame.image.load('grass.png')
+   IMAGE_SMALL = pygame.transform.scale(img, (50, 30))
+   for position in range(20):
+       rand_length = random.randint(0, 700)
+       rand_width = random.randint(0, 400)
+       screen.blit(IMAGE_SMALL, (rand_length, rand_width))
+       pygame.display.flip()
+       #לגרום לדשא לא להיות אחד על השני.
+   running = True
+   while running:
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               running = False
